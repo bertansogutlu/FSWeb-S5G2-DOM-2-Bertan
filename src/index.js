6975189bc47730673f224cff97f2651a3e6b834d
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-01-25 01:19:40 +0300

    destinasyon imgler eklendi

--- src/index.js
+++ src/index.js
@@ -7,3 +7,32 @@ import './less/index.less'
 
 
 // Kodlar buraya gelecek!
+
+const buttons = document.querySelectorAll('.btn');
+console.log(buttons);
+
+const desImg = document.querySelector('.content-destination img');
+
+buttons[0].addEventListener('mouseenter', (event) => {
+    desImg.src = 'https://picsum.photos/id/65/600/200'
+})
+
+buttons[0].addEventListener('mouseleave', (event) => {
+    desImg.src = 'http://localhost:9000/img/destination.jpg'
+})
+
+buttons[1].addEventListener('mouseenter', (event) => {
+    desImg.src = 'https://picsum.photos/id/28/600/200'
+})
+
+buttons[1].addEventListener('mouseleave', (event) => {
+    desImg.src = 'http://localhost:9000/img/destination.jpg'
+})
+
+buttons[2].addEventListener('mouseenter', (event) => {
+    desImg.src = 'https://picsum.photos/id/54/600/200'
+})
+
+buttons[2].addEventListener('mouseleave', (event) => {
+    desImg.src = 'http://localhost:9000/img/destination.jpg'
+})
